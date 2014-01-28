e52926db3700ef6743ae37106c518c7b3797c28d
sinahab <sinahab@gmail.com>
2014-01-28 14:24:54 -0800

    adds title and subtitle to homepage.

--- src/Homepage.java
+++ src/Homepage.java
@@ -1,11 +1,25 @@
 
+import java.awt.Font;
+
 import javax.swing.JLabel;
 
 public class Homepage extends Page {
 	
 	public Homepage() {
+
+		// space component
+        JLabel spacer = new JLabel(" ");
+		
+		// adding the title component
 		JLabel title = new JLabel("Welcome to the Knowledge Base!");
+		title.setFont(new Font("Helvetica", Font.BOLD, 20));
+		
+		// adding the subtitle
+		JLabel subtitle = new JLabel("Knowledge by students for students ...");
+		subtitle.setFont(new Font("Helvetica", Font.BOLD, 18));
+
 		add(title);
-	}		
-	
+		add(spacer);
+		add(subtitle);
+	}	
 }
