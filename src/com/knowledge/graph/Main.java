8754cda5572b4b2332bcecd71d9d1d79b00404da
Thomas Lai <yuyanglai88@gmail.com>
2014-06-10 17:33:33 -0700

    Added JPanel and JButton example

--- src/com/knowledge/graph/Main.java
+++ src/com/knowledge/graph/Main.java
@@ -6,9 +6,8 @@ import javax.swing.SwingUtilities;
 public class Main {
 	public static void main(String[] args) {
 	    SwingUtilities.invokeLater(new Runnable() {
-	        @Override
 	        public void run() {
-	            Homepage homepage = new Homepage();
+	            IndexPage homepage = new IndexPage();
 	            homepage.setVisible(true);
 	        }
 	    });
