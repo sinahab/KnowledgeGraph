8bb4d1d983dadb3f61aa38b417ac67f833d3106d
Thomas Lai <yuyanglai88@gmail.com>
2014-06-11 15:47:17 -0700

    Added IndexTree Panel

--- src/com/knowledge/graph/frontend/IndexPage.java
+++ src/com/knowledge/graph/frontend/IndexPage.java
@@ -1,37 +1,14 @@
 package com.knowledge.graph.frontend;
 
 import javax.swing.BoxLayout;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTree;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
-import javax.swing.tree.DefaultMutableTreeNode;
 
-public class IndexPage extends Page implements TreeSelectionListener {
-	private JTree indexTree;
+public class IndexPage extends Page {
 
 	public IndexPage() {
 		JPanel boxPanel = new JPanel();
 		boxPanel.setLayout(new BoxLayout(boxPanel, BoxLayout.PAGE_AXIS));
 		
-		// Create root node and populate tree
-	    DefaultMutableTreeNode top = new DefaultMutableTreeNode("Knowledge Graph");
-	    populateTree(top);
-	    
-	    indexTree = new JTree(top);
-	    JScrollPane treeView = new JScrollPane(indexTree);
-	    boxPanel.add(treeView);
-	    add(boxPanel);
-	}
-	
-	private void populateTree(DefaultMutableTreeNode top) {
-		
-	}
-
-	public void valueChanged(TreeSelectionEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 }
