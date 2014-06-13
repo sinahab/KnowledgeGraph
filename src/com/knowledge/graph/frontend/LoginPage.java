3989d1e0e8ec85ce917d8ced50d9810b88ca566e
Thomas Lai <yuyanglai88@gmail.com>
2014-06-13 12:13:48 -0700

    Added Insets for better padding of components

--- src/com/knowledge/graph/frontend/LoginPage.java
+++ src/com/knowledge/graph/frontend/LoginPage.java
@@ -3,6 +3,7 @@ package com.knowledge.graph.frontend;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -22,6 +23,7 @@ public class LoginPage extends Page {
 		GridBagConstraints constraint = new GridBagConstraints();
 		constraint.fill = GridBagConstraints.HORIZONTAL;
 		constraint.anchor = GridBagConstraints.CENTER;
+		constraint.ipadx = 10; constraint.ipady = 10;
 		
 		// Student number field
 		JLabel sid_label = new JLabel("Student Number : ", JLabel.RIGHT);
@@ -32,16 +34,19 @@ public class LoginPage extends Page {
 		
 		JTextField sid_field = new JTextField();
 		constraint.gridx = 1; constraint.weightx = 2;
+		constraint.insets = new Insets(0, 0, 0, 30);
 		background.add(sid_field, constraint);
 		
 		// Password field
 		JLabel pass_label = new JLabel("Password : ", JLabel.RIGHT);
 		constraint.gridx = 0; constraint.gridy = 1;
 		constraint.weightx = 1;
+		constraint.insets = new Insets(0, 0, 0, 0);
 		background.add(pass_label, constraint);
 		
 		JPasswordField pass_field = new JPasswordField();
 		constraint.gridx = 1; constraint.weightx = 2;
+		constraint.insets = new Insets(0, 0, 0, 30);
 		background.add(pass_field, constraint);
 		
 		// Login buttons panel
@@ -55,7 +60,8 @@ public class LoginPage extends Page {
 	    });
 		constraint.anchor = GridBagConstraints.LAST_LINE_START;
 		constraint.gridx = 0; constraint.gridy = 2;
-		login_button.setPreferredSize(new Dimension(50,100));
+		constraint.insets = new Insets(10, 10, 10, 5);
+		login_button.setPreferredSize(new Dimension(20,50));
 		background.add(login_button, constraint);
 		
 		JButton cancel_button = new JButton("Cancel");
@@ -68,7 +74,8 @@ public class LoginPage extends Page {
 	    });
 		constraint.anchor = GridBagConstraints.LAST_LINE_END;
 		constraint.gridx = 1;
-		cancel_button.setPreferredSize(new Dimension(50,100));
+		constraint.insets = new Insets(5, 10, 10, 10);
+		cancel_button.setPreferredSize(new Dimension(20,50));
 		background.add(cancel_button, constraint);
 		
 		this.add(background);
