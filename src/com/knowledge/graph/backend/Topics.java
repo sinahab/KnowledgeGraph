346896e359945a8c508830f51f6dc4a5320ce980
Sina Habibian <sinahab@gmail.com>
2014-06-17 22:25:35 -0700

    moves getTopicByID to the pluralized file

--- src/com/knowledge/graph/backend/Topics.java
+++ src/com/knowledge/graph/backend/Topics.java
@@ -27,4 +27,67 @@ public class Topics {
 				}
 		}
 	}
+	
+	public Topic getTopicByID(int t_id){
+		Connection connection = JdbcSqlConnection.getConnection();
+		Topic topic = null;
+		
+		try{
+			Statement statement = connection.createStatement();
+			String query = "SELECT * FROM BelongedTopics WHERE t_id="+t_id;
+			ResultSet rs = statement.executeQuery(query);
+			
+			if(rs.next())
+				topic = new Topic(rs.getNString("t_name"),rs.getNString("description"), rs.getInt("t_id"), rs.getInt("s_id"));
+		}
+		catch(SQLException e){
+			System.out.println("An error occured while searching!");
+			e.printStackTrace();
+		}
+		finally{
+			if(connection!=null)
+				try{
+					connection.close();
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+		}
+		return topic;
+	}
+	
+	public List<Concept> getTiedConcepts(int topic_id){
+		Connection connection = JdbcSqlConnection.getConnection();
+		List<Concept> tied_concepts = new ArrayList<Concept>();
+		
+		try{
+			Statement statement = connection.createStatement();
+			String query = "SELECT * FROM BelongedConcepts WHERE t_id="+topic_id;
+			ResultSet rs = statement.executeQuery(query);
+			
+			while(rs.next()){
+				tied_concepts.add(new Concept(rs.getInt("c_id"), rs.getNString("c_name"), rs.getNString("description"),
+						rs.getInt("t_id")));
+			}
+		}
+		catch(SQLException e){
+			System.out.println("An error occured while searching!");
+			e.printStackTrace();
+		}
+		finally{
+			if(connection!=null)
+				try{
+					connection.close();
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+		}
+		if(tied_concepts.isEmpty())
+			return null;
+		else{
+			return tied_concepts;
+			}
+	}
+
 }
