bbe55cfa70c2d7e739607660b819ddd9108624fe
Czewen <czewengoh@hotmail.com>
2014-06-18 13:19:06 -0700

    changed some stuff

--- src/com/knowledge/graph/backend/Mentors.java
+++ src/com/knowledge/graph/backend/Mentors.java
@@ -58,36 +58,6 @@ public class Mentors {
 		
 	}
 	
-	public boolean approveQuestion(int answer_id, int mentor_id){
-		boolean approval_success = false;
-		Connection connection = JdbcSqlConnection.getConnection();
-		try{
-			Statement statement = connection.createStatement();
-			String update = "INSERT INTO MentorApproves(a_id, student_number) VALUES("+answer_id+", "+mentor_id+");";
-			int success = statement.executeUpdate(update);
-			if(success==1){
-				update = "UPDATE TiedAnswers SET status=approved WHERE a_id"+answer_id+";";
-				success = statement.executeUpdate(update);
-				//Check whether the approval was succesful
-				if(success==1)
-					approval_success = true;
-			}
-		}
-		catch(SQLException e){
-			System.out.println("An error occured while updating or the answer doesn't exist!");
-			e.printStackTrace();
-		}
-		finally{
-			if(connection!=null)
-				try{
-					connection.close();
-				}
-				catch(Exception e){
-					e.printStackTrace();
-				}
-		}
-		return approval_success;
-	}
 	
 	public Mentor getMentorByID(int mentor_sid){
 		Mentor mentor = null;
